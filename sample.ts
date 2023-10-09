5e035202a558d4642159ce412622758dd27572fe
in-ch <sxin2949@naver.com>
2023-10-09 20:06:31 +0900

    feat(해싱 함수): 암호 찾기

--- sample.ts
+++ sample.ts
@@ -1,3 +1,11 @@
-function 솔루션() {}
+function 솔루션() {
+  let a = { name: "kim" };
+  let b = a;
+
+  a.name = "lee";
+
+  console.log(a);
+  console.log(a === b);
+}
 
 솔루션();
---
+++ 문법/해싱/암호찾기.js
@@ -0,0 +1,41 @@
+// https://www.acmicpc.net/problem/21559
+
+/**
+ * @param inputStr 입력받은 문자열
+ * @param n 자를 문자 수
+ * @description 입력받은 문자열에서 n을 받아서 n길이만큼 잘라서 배열을 리턴한다.
+ * @return 자른 문자열들을 담은 배열
+ */
+ function generateSubstrings(inputStr, n) {
+    const substrings = [];
+  
+    for (let i = 0; i <= inputStr.length - n; i++) {
+      const substring = inputStr.slice(i, i + n);
+      substrings.push(substring);
+    }
+  
+    return substrings;
+  }
+  
+  /**
+   * @param 문자열의 길이 N과 뽑아낼 수의 자릿수 K
+   */
+  function 암호찾기(NK, A, B) {
+    let count = 0;
+    const k = NK.split(" ")[1] * 1;
+    const aSet = new Set();
+  
+    const splitedA = generateSubstrings(A, k);
+    const splitedB = generateSubstrings(B, k);
+  
+    splitedA.forEach((_a) => aSet.add(_a));
+    splitedB.forEach((_b) => aSet.has(_b) && count++);
+  
+    return count;
+  }
+  
+  
+console.log(암호찾기("4 2", "1122", "6677")); // 0 
+console.log(암호찾기("3 1", "122", "221")); // 2
+console.log(암호찾기("3 2", "124", "248")); // 1 
+  

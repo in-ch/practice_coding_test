07185f6daed0b1709e4fdfad1485cd183e13d40b
in-ch <sxin2949@naver.com>
2023-10-24 12:23:42 +0900

    feat(해싱): 의상입기 예제 추가

---
+++ 문법/해싱/의상입기.js
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42578
+// Lv2 
+
+function solution(clothes) {
+    const len = clothes.length;
+    let answer = 1;
+    let obj = {};
+    
+    for(let i=0; i<len; i++){
+        obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+        console.log('sex');
+    }
+    for(let key in obj){
+        answer *= obj[key];
+    }
+    
+    return answer-1;
+}
+
+console.log(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]));
+console.log(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]));

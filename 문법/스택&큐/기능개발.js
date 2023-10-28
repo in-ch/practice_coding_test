534a560f6b35caa6cca469e4837125ea41d107b8
in-ch <sxin2949@naver.com>
2023-10-28 17:59:02 +0900

    feat(스택 & 큐): 프로세스 예제 추가

--- 문법/스택&큐/기능개발.js
+++ 문법/스택&큐/기능개발.js
@@ -1,29 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42586
 // Lv2
 
-// 내가 적은 코드
-// function solution(progresses, speeds) {
-//     var answer = [];
-//     var _progresses = [...progresses];
-
-//     while(_progresses.length > 0) {
-//         _progresses.forEach((_, i) => {
-//             _progresses[i] = _progresses[i] + speeds[i];
-//         });
-//         let doneCount = 0;
-//         for(let i=0; i < _progresses.length; i++){
-//             if(_progresses[i]> 99) {
-//                 doneCount++;
-//             } else break;
-//         }
-//         if(doneCount > 0) {
-//             answer.push(doneCount);
-//             for(let i = 0; i < doneCount; i++) _progresses.shift();
-//         } 
-//     }
-//     return answer;
-// }
-
 function solution(progresses, speeds) {
     var answer = [];
     const developAccesses = progresses.map((progress,idx)=> Math.ceil((100 - progress) / speeds[idx]))
---
+++ 문법/스택&큐/프로세스.js
@@ -0,0 +1,34 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+// Lv 2 
+
+// 우선순위와 idx를 담은 배열 형태의 dataset 변수 생성
+// 우선순위를 체크하고 문제가 없다면 해당 우선순위를 1로 설정 (copyPriorities)
+// location이 idx와 맞으면 break로 빠르게 종료
+// 주의할 점 : copyPriorities[idx] = 1 대신 copyPriorities.splice(idx,1)이 물론 간단 테스트케이스에서 가능은 합니다만
+// Solve는 불가능한 시간초과가 발생하게 되므로 가능한 접근하여 처리하는 것을 추천합니다.
+function solution(priorities, location) {
+    const copyPriorities = [...priorities]
+    const dataset = priorities.map((priority,idx)=>({priority,idx}))
+    let excuteCount = 0
+
+    while(dataset.length !== 0){
+        const maxValue = Math.max(...copyPriorities)
+        const {priority,idx} = dataset.shift();
+        // 같으면 실행 순서를 증가시킨다. 
+        if(priority >= maxValue){
+            excuteCount++
+            copyPriorities[idx] = 0
+            if(idx === location){
+                break;
+            }
+        }
+        else{
+            dataset.push({priority,idx})
+        }
+    }
+    return excuteCount
+}
+
+
+console.log(solution([2, 1, 3, 2], 2));
+// console.log(solution([1, 1, 9, 1, 1, 1], 0));

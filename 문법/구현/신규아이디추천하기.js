5fd074c2d83b45a3bd8c3a6cca809f540d3036cd
in-ch <sxin2949@naver.com>
2023-12-31 15:35:55 +0900

    feat(해싱): 순위 검색

---
+++ 문법/구현/신규아이디추천하기.js
@@ -0,0 +1,10 @@
+function solution(new_id) {
+    var answer = '';
+    return answer;
+}
+
+console.log(solution("...!@BaT#*..y.abcdefghijklm")); // bat.y.abcdefghi
+console.log(solution("z-+.^.")); // z--
+console.log(solution("=.=")); // aaa
+console.log(solution("123_.def")); // 123_.def
+console.log(solution("abcdefghijklmn.p")); // abcdefghijklmn
---
+++ 문법/해싱/순위 검색.js
@@ -0,0 +1,125 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/72412
+// Lv 2
+
+function mySolution(infos, querys) {
+    const answers = [];
+
+    function createObject(languages, positions, careers, foods) {
+        const obj = {};
+
+        for (const lang of languages) {
+            obj[lang] = {};
+            for (const position of positions) {
+                obj[lang][position] = {};
+                for (const career of careers) {
+                    obj[lang][position][career] = {};
+                    for (const food of foods) {
+                        obj[lang][position][career][food] = { scores: [] };
+                    }
+                }
+            }
+        }
+
+        return obj;
+    }
+
+    const languages = ["java", "cpp", "python", "-"];
+    const positions = ["frontend", "backend", "-"];
+    const careers = ["senior", "junior", "-"];
+    const foods = ["pizza", "chicken", "-"];
+
+    const obj = createObject(languages, positions, careers, foods);
+    
+    function dfs(keys, index) {
+        if (index === keys.length - 1) {
+            obj[keys[0]][keys[1]][keys[2]][keys[3]]["scores"].push(keys[4]);
+            return;
+        }
+        const newKeys = [...keys];
+        newKeys[index] = "-";
+        dfs(newKeys, index + 1);
+        dfs(keys, index + 1);
+    }
+
+    for (const info of infos) {
+        const [lang, position, career, food, score] = info.split(" ");
+        obj[lang][position][career][food]['scores'].push(Number(score));
+        dfs([lang, position, career, food, Number(score)], 0);
+    }
+
+    for (const query of querys) {
+        let answer = 0;
+        const [lang, position, career, food, score] = query.replace(/and /g, "").split(" ");
+        const targetScores = (lang === "-" && position === "-" && career === "-" && food === "-") ? obj[lang][position][career][food].scores : [...new Set(obj[lang][position][career][food].scores)];
+        const targetScore = Number(score);
+        for (const s of targetScores) {
+            if (score === "-" || s >= targetScore) {
+                answer++;
+            }
+        }
+
+        answers.push(answer);
+    }
+
+    return answers;
+}
+
+function solution(infos, querys) {
+    const rule = new Map();
+    infos.forEach(v => {
+        const info = v.split(' ');
+        const score = Number(info.pop());
+
+        let key = info.join(''); 
+        rule.set(key, rule.has(key) ? [...rule.get(key), score] : [score]);
+    });
+
+    /// 이진 탐색을 하기 위해 정렬을 한다.
+    for(let [key, value] of rule){
+        rule.set(key, value.sort((a, b) => a - b));
+    }
+
+    return querys.map(e => {
+        const conditions = e.split(/ and | |-/i).filter(e => e);
+        return search(rule, conditions);
+    });
+}
+
+const search = (rule, conditions) => {
+    const score = conditions.pop();
+    return Array.from(rule.keys())
+        .filter(key => conditions.every(v => key.includes(v)))
+        .reduce((a, c) => a + rule.get(c).slice(lowerBound(rule.get(c), score)).length, 0);
+}
+
+const lowerBound = (arr, target) => {
+    let left = 0;
+    let right = arr.length; 
+    while(left < right){
+        const mid = Math.floor((left + right) / 2);
+
+        if(arr[mid] >= target) right = mid;
+        else left = mid + 1;
+    }
+
+    return left;
+}
+    
+console.log(solution(
+    [
+        "java backend junior pizza 150",
+        "python frontend senior chicken 210",
+        "python frontend senior chicken 150",
+        "cpp backend senior pizza 260",
+        "java backend junior chicken 80",
+        "python backend senior chicken 50"
+    ], 
+    [
+            "java and backend and junior and pizza 100",
+            "python and frontend and senior and chicken 200",
+            "cpp and - and senior and pizza 250",
+            "- and backend and senior and - 150",
+            "- and - and - and chicken 100",
+            "- and - and - and - 150"
+    ]
+)); // [1,1,1,1,2,4]
